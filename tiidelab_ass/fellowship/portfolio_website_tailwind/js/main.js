160cbe46852495cdf5f599859225f237646399ec
ceceukoha <ceceukoha@gmail.com>
2023-08-01 20:44:30 +0100

    Added larger screen view

---
+++ tiidelab_ass/fellowship/portfolio_website_tailwind/js/main.js
@@ -0,0 +1,11 @@
+
+
+// $('#nav-items a, #btn').on('click', function (e) { 
+//     if (this.hash !== '') {
+//         e.preventDefault();
+//         console.log("no");
+
+//     } else {
+//         console.log("no");
+//     }
+// })
